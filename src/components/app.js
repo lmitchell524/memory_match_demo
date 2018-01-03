d85d518a9b5437bb339906d45a73e01c24f91b65
lmitchell524 <lmitchell524@gmail.com>
2018-01-03 14:58:20 -0800

    initial setup and card creation

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 import '../assets/css/app.css';
-import logo from '../assets/images/logo.svg';
+import cardData from '../assets/helpers/card_data';
+import Card from './card';
 
-const App = () => (
-    <div className="app">
-        <h1>Memory Match</h1>
-    </div>
-);
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            cards: cardData
+        }
+
+        this.flipCard = this.flipCard.bind(this);
+
+    }
+
+    flipCard(index) {
+        const newCards = this.state.cards.slice();
+
+        newCards[index].flipped = !newCards[index].flipped;
+        //toggles to opposite of what it was
+
+        this.setState({
+            cards: newCards
+        });
+
+    }
+
+    render(){
+
+        const cardElements = this.state.cards.map((card, index) => {
+            return <Card key={index} flip={() => this.flipCard(index)} card={card} />
+        });
+        //put flipcard in anon function b/c it's going to be called later
+
+        return(
+            <div className="app">
+                <h1>Hello</h1>
+                {cardElements}
+            </div>
+        )
+    };
+}
 
 export default App;
